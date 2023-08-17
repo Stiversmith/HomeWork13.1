abf2687c8a4088ceabfe9f8ee7dd82cb0058548a
Aliaksandr Yashchuk <alexander@stiversmith.com>
2023-08-16 21:02:08 -0400

    Done

--- HomeWork13.1/HomeWork13.1/ViewController.swift
+++ HomeWork13.1/HomeWork13.1/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // #1
+        // #1 - $44
         // #2
         // #3
         // #4
